bd48e9b95af5d8a4b88c4ba4d2c48561e34c9118
Aditya <adityaalt9090@gmail.com>
2025-10-27 23:30:53 +0530

    36 min of tutorial done and page 1 made

---
+++ script.js
@@ -0,0 +1,10 @@
+gsap.to("#nav",{
+    backgroundColor : "#000",
+    height: "120px",
+    duration: 0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller: "body",
+        markers: true,
+    }
+})
